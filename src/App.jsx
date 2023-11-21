73a51286d4b913ba68914e566b2b0e6862b2f419
YinwolfB <comandoxrp@hotmal.com>
2023-11-20 21:48:02 -0500

    se crea el hook useCrud

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useEffect } from 'react'
 import './App.css'
+import { useCrud } from './hooks/useCrud'
 
 function App() {
+  
+  const url = 'https://user-crud.academlo.tech'
+const [ users, getUsers,createUser,delateUser, updateUser ] = useCrud()
+
+useEffect(() => {
+  getUsers()
+}, [])
 
 
   return (
     <div>
-      <h1>Hola</h1>
+      <h1>Users</h1>
+
     </div>
   )
 }
---
+++ src/hooks/useCrud.js
@@ -0,0 +1,52 @@
+import axios from "axios"
+import { useState } from "react"
+
+export const useCrud = (baseUrl) => {
+    const [infoApi, setInfoApi] = useState()
+
+    //GET -- Obtener ,leer o acceder a la base de datos
+
+    const getApi = (path) => {
+        const url = `${baseUrl}${path}/`
+        axios.get(url)
+            .then(res => setInfoApi(res.data))
+            .catch(err => console.log(err))
+    }
+
+    //POST -- Crear un nuevo registro en la base de datos
+    const postApi = (path, data) => {
+        const url = `${baseUrl}${path}/`
+        axios.post(url, data)
+            .then(res => {
+                console.log(res.data)
+                setInfoApi(...infoApi, res.data)
+            })
+            .catch(err => console.log(err))
+    }
+
+    //DELATE -- elimina un registro existente en la base de datos
+
+    const delateApi = (path, id) => {
+        const url = `${baseUrl}${path}/${id}/`
+        axios.delete(url)
+            .then(res => {
+                setInfoApi(infoApi.filter(e => e.id !== id))
+            })
+            .catch(err => console.log(err))
+    }
+
+    //UPDATE -- actualiza un registro en la base de datos
+
+    const updateApi = (path, id, data) => {
+        const url = `${baseUrl}${path}/${id}/`
+        axios.patch(url, data)
+            .then(res => {
+                console.log(res.data)
+                setInfoApi(infoApi.map(e => e.id === id ? res.data : id))
+            })
+            .catch(err => console.log(err))
+    }
+
+    //
+    return [infoApi, getApi, postApi, delateApi, updateApi]
+}

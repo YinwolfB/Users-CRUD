f1229ec83531910fafe49751189f8bc8f0bae878
YinwolfB <comandoxrp@hotmal.com>
2023-11-21 20:09:21 -0500

    Se crea la caja

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -13,10 +13,10 @@ export const UserCard = ({ user, delateUser, setInfoUpdate,setIsDisable }) => {
 
     return (
         <article>
-            <h3>{user.first_name}{user.last_name}</h3>
+            <h3>{user.first_name} {user.last_name}</h3>
             <ul>
-                <li><span>Email</span><span>{user.email}</span></li>
-                <li><span>Birthday</span><span>{user.birthday}</span></li>
+                <li><span>Email: </span><span>{user.email}</span></li>
+                <li><span>Birthday: </span><span>{user.birthday}</span></li>
             </ul>
             <button onClick={handleDelate}><i className='bx bxs-trash-alt'></i></button>
             <button onClick={handelEdit}><i className='bx bxs-edit' ></i></button>
